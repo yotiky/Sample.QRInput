3b799617e4f6f0dccc39f8e9f9b8b4d3c7f5e824
yotiky <yotiky@gmail.com>
2020-10-23 00:10:32 +0900

    feat: QR scan sample.

---
+++ Assets/Scripts/QRVisualizer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+public class QRVisualizer : MonoBehaviour
+{
+    public TextMeshPro textMeshPro;
+    private QRScanner qRScanner;
+
+    void Start()
+    {
+        textMeshPro.text = "";
+
+        qRScanner = GetComponent<QRScanner>();
+        qRScanner.OnScanned
+            .Subscribe(qr =>
+            {
+                Debug.Log(qr.Data);
+                textMeshPro.text = qr.Data;
+            })
+            .AddTo(this);
+        qRScanner.IsReady
+            .Where(x => x)
+            .First()
+            .Subscribe(_ => qRScanner.StartScan())
+            .AddTo(this);
+    }
+}
